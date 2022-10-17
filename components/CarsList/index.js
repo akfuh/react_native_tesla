43f31f983be59140a67acce0ac9b7256a0ff0fba
tofunaor <85100859+tofunaor@users.noreply.github.com>
2022-10-17 14:04:00 -0700

    Add files via upload

---
+++ components/CarsList/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, FlatList, Dimensions } from "react-native";
+import CarItem from "../CarItem";
+
+import styles from "./styles";
+import cars from "./cars";
+
+const CarsList = (props) => {
+  const { name, tagline, image, taglineCTA } = props.car;
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={cars}
+        renderItem={({ item }) => <CarItem car={item} />}
+        snapToAlignment={"start"}
+        decelerationRate={"fast"}
+        snapToInterval={Dimensions.get("window").height}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+export default CarsList;
